4b58c5bd46988299347beea9d34ac12c49fbd0e8
NL <nl@develup.pro>
2025-11-06 00:39:42 +0200

    feat: remember window pos

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,12 +1,15 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri_plugin_window_state::Builder as WindowStateBuilder;
+
 mod instances;
 mod utils;
 use crate::instances::{instance_get, instance_start, instance_stop, instances_list};
 
 fn main() {
     tauri::Builder::default()
+        .plugin(WindowStateBuilder::default().build())
         .invoke_handler(tauri::generate_handler![
             instance_get,
             instance_start,
